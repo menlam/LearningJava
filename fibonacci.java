dff54a94bb4f1de96b401b57ab6b7d4b5744c31e
Menlam <official.menlam@gmail.com>
2019-07-05 10:18:11 +0600

    fibonacci series without recurssion

--- fibonacci.java
+++ fibonacci.java
@@ -1,23 +1,21 @@
-//fibonaccie series till 10 without using recurssion
 class fibonacci{
 	
 	public static void main(String args[])
 	{
 		
- 		
 
 		int a=0;
 		int b=1;
 		int sum=a+b;
 		int i=0;
-		System.out.print(a+" ");
-		System.out.print(b+" ");
+		System.out.println(a);
+		System.out.println(b);
 		do{
-			System.out.print(sum+" ");
+			System.out.println(sum);
 			a=b;
 			b=sum;
 			sum=a+b;
 			i++;
-		}while(i<=10);
+		}while(i<=5);
 		}
 	}
